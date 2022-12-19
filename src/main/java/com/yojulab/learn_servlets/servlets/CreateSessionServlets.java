09259aa2af46bd7e1855a6592f7ffeaabe7448ee
yojulab <otter.oh@gmail.com>
2022-12-19 15:20:59 +0900

    add session

---
+++ src/main/java/com/yojulab/learn_servlets/servlets/CreateSessionServlets.java
@@ -0,0 +1,38 @@
+package com.yojulab.learn_servlets.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+// /session/createServlets?username=yojulab&password=1234
+@WebServlet(urlPatterns="/session/createServlets")
+public class CreateSessionServlets extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // display
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<div>Create Session Servlets</div>");
+        // login 
+        if(username.equals("yojulab") && password.equals("1234")){
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute("username", username);
+            httpSession.setAttribute("password", password);
+            printWriter.println("<div>"+username+", "+password+"</div>");
+        } else {
+            printWriter.println("<div>Faild</div>");
+        }
+        printWriter.close();
+    }
+}
