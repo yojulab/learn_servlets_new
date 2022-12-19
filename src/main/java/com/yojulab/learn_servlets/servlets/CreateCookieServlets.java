e3e297a7e405ca3c67a9a05f9d8a3f937fbf156d
yojulab <otter.oh@gmail.com>
2022-12-19 14:59:58 +0900

    add cookies

---
+++ src/main/java/com/yojulab/learn_servlets/servlets/CreateCookieServlets.java
@@ -0,0 +1,29 @@
+package com.yojulab.learn_servlets.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/cookies/createServlets")
+public class CreateCookieServlets extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Cookie
+        Cookie cookie_first = new Cookie("firstName", "Yoju");
+        Cookie cookie_second = new Cookie("secondName", "Lab");
+        response.addCookie(cookie_first);
+        response.addCookie(cookie_second);
+        // display
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<div>CreateCookieServlets</div>");
+        printWriter.close();
+    }
+}
---
+++ src/main/java/com/yojulab/learn_servlets/servlets/GetAndDeleteCookieServlets.java
@@ -0,0 +1,40 @@
+package com.yojulab.learn_servlets.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/cookies/getAndDeleteServlets")
+public class GetAndDeleteCookieServlets extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // display
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<div>Get and Delete Cookie Servlets</div>");
+
+        // get Cookie
+        Cookie cookies[] = request.getCookies();
+        for(Cookie cookie : cookies) {
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            printWriter.println("<div>Cookie Name : "+name+", value : "+value+" </div>");
+
+            // delete cookie
+            if(name.equals("secondName")){
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+
+        }
+
+        printWriter.close();
+    }
+}
